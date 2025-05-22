55eacaefb5e19410db92359d3536c6962a1a6062
JoshBudzynski <157167337+JoshBudzynski@users.noreply.github.com>
2025-05-22 15:00:30 -0500

    Quality of Life

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Order(db.Model):
+    __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     event = db.Column(db.String(100))
